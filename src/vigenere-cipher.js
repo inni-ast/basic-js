d4a28a6a1cf17086a3592e598394594b1a8f39f3
inni-ast <inni_@tut.by>
2022-10-13 15:53:46 +0300

    feat: last done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(...args) {
+    if (args.length !== 2 || typeof args[0] !== "string" || typeof args[1] !== "string") throw new Error("Incorrect arguments!");
+    let message = args[0].toUpperCase();
+    let key = args[1].toUpperCase();
+    let result = "";
+    while (message.length > key.length) {
+      key += key;
+    }
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let char = this.alphabet.slice(this.alphabet.indexOf(key[j])) + this.alphabet;
+        result += char[this.alphabet.indexOf(message[i])];
+      } else {
+        result += message[i];
+        j--;
+      }
+      j++;
+    }
+    if (this.type) {
+      return result;
+    } else {
+      return result.split('').reverse().join("");
+    }
+  }
+  decrypt(...args) {
+    if (args.length !== 2 || typeof args[0] !== "string" || typeof args[1] !== "string") throw new Error("Incorrect arguments!");
+    let message = args[0].toUpperCase();
+    let key = args[1].toUpperCase();
+    let result = "";
+    while (message.length > key.length) {
+      key += key;
+    }
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let char = this.alphabet.slice(this.alphabet.indexOf(key[j])) + this.alphabet;
+        result += this.alphabet[char.indexOf(message[i])];
+      } else {
+        result += message[i];
+        j--;
+      }
+      j++;
+    }
+    if (this.type) {
+      return result;
+    } else {
+      return result.split('').reverse().join("");
+    }
+
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
